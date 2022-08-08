9d075a5c86b2a2d99dce58790742706b13c68546
DESKTOP-R2KNVBC\yggb1 <yggb159@naver.com>
2022-08-09 01:06:05 +0900

    JPA 엔티티 및 MemberService, MemberRepo 개발
    JPA 적용 및 회원 도메인 개발

---
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    @Rollback(value = false)
+    void 회원가입() {
+
+        //given
+        Member member = new Member();
+        member.setName("Kim");
+
+        //when
+        Long savedId = memberService.join(member);//회원가입
+
+        //then
+        em.flush();
+        assertEquals(member,memberRepository.findOne(savedId));
+    }
+}
